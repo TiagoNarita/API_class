89d976b6e6a49be417e9bd35f590b02c5230f106
Tiago <tiagobnarita@gmail.com>
2024-04-01 22:12:30 -0300

    uptade api

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const server = express();
 
-//localhost:3000/curso
-server.get("/curso", () => {
-  console.log("acessou a rota");
+//localhost:3000/curso/2
+server.get("/curso", (req, res) => {
+  const id = req;
 });
 
 server.listen(3000);
